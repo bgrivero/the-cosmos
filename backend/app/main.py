e153a9e8f2b0e670d8650b39699eea9a4b97b83a
nclvrl <nicolecdverial@gmail.com>
2026-06-09 22:48:35 +0800

    chore: register auth router and add bcrypt jose dependencies

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,20 +1,25 @@
-from fastapi import FastAPI, HTTPException, Depends, Query
-from pydantic import BaseModel, Field
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
-from external_api import search_vn, get_vn
+from app.routers.auth import router as auth_router
+from app.models.user import User
+from app.database import Base, engine
+# from external_api import search_vn, get_vn
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
 origins = [
-    "http://127.0.0.1:5173",    
-    "http://localhost:5173" 
+    "http://127.0.0.1:5173",
+    "http://localhost:5173"
 ]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,            
-    allow_credentials=True,          
-    allow_methods=["*"],              
-    allow_headers=["*"],              
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-
+app.include_router(auth_router)
